3e8e3bcdd0793c9c6261b11332cee2ec90d9449b
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2024-02-07 18:40:27 +0200

    edit name prop in person class

--- ReflectionInC#/Program.cs
+++ ReflectionInC#/Program.cs
@@ -45,7 +45,7 @@
     }
     public class Person
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Age { get; set; }
         public decimal Height { get; set; }
     }
